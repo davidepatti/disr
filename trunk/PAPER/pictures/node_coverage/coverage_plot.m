25ec8fee71d75e8a0d8abc56a56320312b274afc
xedivad@gmail.com <xedivad@gmail.com@e11a83a3-9fd8-df8c-87f1-d18b88243838>
2014-07-08 13:11:30 +0000

    Fixed pics for review

--- trunk/PAPER/pictures/node_coverage/coverage_plot.m
+++ trunk/PAPER/pictures/node_coverage/coverage_plot.m
@@ -1,12 +1,13 @@
 %after that the workspace is loaded from file
 hold on;
-plot(def_node,ideal,'-');
+plot(def_node,ideal,'r--');
+plot(def_node, trenta,'-+');
 plot(def_node, trenta,'--o');
-plot(def_node,cinquanta,':.');
-plot(def_node,cento,'-.x');
+plot(def_node,cinquanta,':*');
+plot(def_node,cento,'-.');
 %plot(def_node,ottanta,':');
 
 
 xlabel('Node Defect Rate');
 ylabel('Node Coverage');
-legend('Ideal','30x30','50x50','100x100');
+legend('Ideal','10x10','30x30','50x50','100x100');
